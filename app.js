2ff7e2b7cb019e84701b85f18ad1b3b20ce70789
Anurag <aarwalka@cisco.com>
2020-09-12 02:07:46 +0530

    added web sockets

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const path = require('path');
 
+const express = require('express')
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.get('/', (req, res) => {
+  res.send('App is running...')
+});
 
+io.on('connection', socket => {
+  socket.emit('request', /* … */); // emit an event to the socket
+  io.emit('broadcast', /* … */); // emit an event to all connected sockets
+  socket.on('reply', () => { /* … */ }); // listen to the event
+});
 
-module.exports = app;
+server.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
