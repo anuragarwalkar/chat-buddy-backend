534165cc66996cb1b0c8e745850638f55cf67165
Anurag Arwalkar <anurag.arwalkar@globant.com>
2023-02-18 09:37:12 +0530

    updated

--- src/config/mongoDB.ts
+++ src/config/mongoDB.ts
@@ -6,7 +6,7 @@ const dBConnection = async () =>{
     try {
         const { mongoUserName, mongoPassword } = config as Config;
         const options = {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true, useFindAndModify:false };
-        const connection = await mongoose.connect(`mongodb+srv://${mongoUserName}:${mongoPassword}@cluster0-mbclo.gcp.mongodb.net/chatBuddy?retryWrites=true&w=majority`, options);
+        const connection = await mongoose.connect(`mongodb+srv://${mongoUserName}:${mongoPassword}@cluster0.gxn0le4.mongodb.net/?retryWrites=true&w=majority`, options);
         console.log(`Mongo Db connected to ${connection.connection.host}`)  
     } catch (error) {
         console.log(error.message)
